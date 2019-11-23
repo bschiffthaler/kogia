272570b489044f17bb6f18d753afc19b08119374
Bastian Schiffthaler <b.schiffthaler@gmail.com>
2019-11-23 12:48:17 +0100

    Improved script wrapper

--- scripts/generic_wrapper.sh
+++ scripts/generic_wrapper.sh
@@ -3,12 +3,27 @@
 # Call this script as you would the bwa executable. It acts as a thin layer
 # around the docker containers in kogia. Need bash 4+
 
-set -eo pipefail
+set -euo pipefail
 
 # This is the most crossplatform way I can come up with to resolve a symlink.
 # If you have something more concise that doesn't rely on python I'm all for it
 function rl() {
   /usr/bin/env python3 -c 'import os, sys; print(os.path.realpath(sys.argv[1]))' $1
+}
+
+function get_uid() {
+  /usr/bin/env python3 -c 'import os; print(os.getuid())'
+}
+
+function get_gid() {
+  /usr/bin/env python3 -c 'import os; print(os.getgid())'
+}
+
+# Don't mess with the indentation...
+function get_suppl_groups() {
+  /usr/bin/env python3 -c \
+'import os
+print(",".join([str(x) for x in os.getgroups() if not x == os.getgid()]))'
 }
 
 function gen_docker() {
@@ -37,7 +52,10 @@ function gen_docker() {
       MOUNTS[$arg]=$arg
     fi
   done
-  cmd="docker run -w $pw --rm --user ${UID}:${GID} -i"
+  MYUID=$(get_uid)
+  MYGID=$(get_gid)
+  MYSUPPGRPS=$(get_suppl_groups)
+  cmd="docker run -w $pw --rm --user ${MYUID}:${MYGID} --group-add ${MYSUPPGRPS} -i"
   for M in ${MOUNTS[@]}
   do
     cmd="$cmd -v ${M}:${M}"
